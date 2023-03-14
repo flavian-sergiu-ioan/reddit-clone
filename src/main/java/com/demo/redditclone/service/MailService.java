8a8e9875365ac001160926a48d62780cda135657
flavian-sergiu-ioan <flavian.ioan@gmail.com>
2023-03-14 07:56:17 +0200

    User Registration and send email via Mailtrap

---
+++ src/main/java/com/demo/redditclone/service/MailService.java
@@ -0,0 +1,41 @@
+package com.demo.redditclone.service;
+
+import com.demo.redditclone.exceptions.SpringRedditException;
+import com.demo.redditclone.model.NotificationEmail;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class MailService {
+
+    private final JavaMailSender mailSender;
+    private final MailContentBuilderService mailContentBuilder;
+
+    @Async
+    void sendMail(NotificationEmail notificationEmail) {
+        MimeMessagePreparator messagePreparator = mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setFrom("springreddit@email.com");
+            messageHelper.setTo(notificationEmail.getRecipient());
+            messageHelper.setSubject(notificationEmail.getSubject());
+            messageHelper.setText(mailContentBuilder.build(notificationEmail.getBody()));
+        };
+        try {
+            mailSender.send(messagePreparator);
+            log.info("Activation email sent!!");
+        } catch (MailException e) {
+            log.error("Exception occurred when sending mail", e);
+            throw new SpringRedditException("Exception occurred when sending mail to " + notificationEmail.getRecipient(), e);
+        }
+    }
+}
