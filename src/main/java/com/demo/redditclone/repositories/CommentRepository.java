6e7737ffa5ee755810f20e9e8d2a262f439ab66f
flavian-sergiu-ioan <flavian.ioan@gmail.com>
2023-03-22 07:21:50 +0200

    add comments functionalities

--- src/main/java/com/demo/redditclone/repositories/CommentRepository.java
+++ src/main/java/com/demo/redditclone/repositories/CommentRepository.java
@@ -1,7 +1,13 @@
 package com.demo.redditclone.repositories;
 
 import com.demo.redditclone.model.Comment;
+import com.demo.redditclone.model.Post;
+import com.demo.redditclone.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByPost(Post post);
+
+    List<Comment> findAllByUser(User user);
 }
